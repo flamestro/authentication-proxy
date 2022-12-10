3f535bdeaf27c46b39e57d3c6fa601582215e621
flamestro <noreplyflamestro@gmail.com>
2022-12-10 18:02:51 +0100

    Add max attempts

--- src/authenticator.middleware.ts
+++ src/authenticator.middleware.ts
@@ -3,14 +3,18 @@ import { NextFunction, Request, Response } from 'express';
 import { loginView } from './view/loginView';
 import { randomUUID } from 'crypto';
 
+const ttlForTries = 6000000; // 100 mins
+let nextCleanForTries = createDateInTheFuture(ttlForTries);
 const ttlForAuthorizedUsersList = 1200000; // 20 mins
+const maxLoginAttempts = 10;
 const authorizedUsers = [];
+let loginAttempts = 0;
 let nextCleanOfAuthorizedUsers = createDateInTheFuture(
   ttlForAuthorizedUsersList,
 );
 
 function createDateInTheFuture(futureMs: number) {
-  return new Date(Date.now() + futureMs,);
+  return new Date(Date.now() + futureMs);
 }
 
 function clearAuthorizedUsersList() {
@@ -30,9 +34,16 @@ export class AuthenticatorMiddleware implements NestMiddleware {
       );
       clearAuthorizedUsersList();
     }
+    const shouldCleanLoginAttempts =
+      nextCleanForTries.getTime() <= new Date(Date.now()).getTime();
+    if (shouldCleanLoginAttempts) {
+      nextCleanForTries = createDateInTheFuture(ttlForTries);
+      loginAttempts = 0;
+    }
     if (
+      loginAttempts < maxLoginAttempts &&
       req.headers['authorization'] ===
-      `Basic ${process.env.USER_NAME}:${process.env.PASSWORD}`
+        `Basic ${process.env.USER_NAME}:${process.env.PASSWORD}`
     ) {
       const userId = randomUUID();
       authorizedUsers.push(userId);
@@ -43,12 +54,14 @@ export class AuthenticatorMiddleware implements NestMiddleware {
       });
       next();
     } else if (
+      loginAttempts < maxLoginAttempts &&
       req.cookies &&
       req.cookies['authorization'] &&
       authorizedUsers.includes(req.cookies['authorization'])
     ) {
       next();
     } else {
+      loginAttempts += 1;
       return res.send(loginView);
     }
   }
