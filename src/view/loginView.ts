541c96a82b579361a25fa9d50d45da5453d29d9f
flamestro <noreplyflamestro@gmail.com>
2023-04-08 17:58:13 +0200

    update view for mobile

--- src/view/loginView.ts
+++ src/view/loginView.ts
@@ -1,17 +1,17 @@
 export const loginView = `
-<div style="display: flex; flex-direction: column; align-items: center; width: 100%; height: 100%;">
+<div style="display: flex; flex-direction: column; align-items: center; width: 100%; height: 100%;  font-size: 2rem; ">
     <div>
         You are not authorized to access this page.
     </div>
     <div>
-        <form style="display: flex; flex-direction: column; align-items: center; width: 200px; height: 200px;" method="post" 
+        <form style="display: flex; flex-direction: column; align-items: center; width: 30rem; height: 30rem;" method="post" 
         onsubmit="fetch(document.location.origin, {credentials: 'include',
                                                     headers: {
                                                       'authorization': 'Basic ' + document.getElementById('username').value + ':' + document.getElementById('password').value
                                                     },}).then(_ => location.reload()); return false;">
-            <input style="width: 200px; height: 50px; border-radius: 15px; margin: 10px;" type="text" name="username" id="username" placeholder="Username"/>
-            <input style="width: 200px; height: 50px; border-radius: 15px; margin: 10px;"  type="password" name="password" id="password" placeholder="Password"/>
-            <input style="width: 100px; height: 50px; border-radius: 15px; margin: 10px;"  type="submit" value="Login" />
+            <input style="width: 30rem; height: 5rem; border-radius: 15px; margin: 10px; font-size: 2rem;" type="text" name="username" id="username" placeholder="Username"/>
+            <input style="width: 30rem; height: 5rem; border-radius: 15px; margin: 10px; font-size: 2rem;"  type="password" name="password" id="password" placeholder="Password"/>
+            <input style="width: 30rem; height: 5rem; border-radius: 15px; margin: 10px; font-size: 2rem;"  type="submit" value="Login" />
         </form>
     </div>
 </div>
