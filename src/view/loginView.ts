68fccee8bb4bde75693d35f5c091a77ab0e015d0
flamestro <noreplyflamestro@gmail.com>
2023-04-09 11:50:38 +0200

    Adjust style #hakky

--- src/view/loginView.ts
+++ src/view/loginView.ts
@@ -1,18 +1,59 @@
 export const loginView = `
-<div style="display: flex; flex-direction: column; align-items: center; width: 100%; height: 100%;  font-size: 2rem; ">
+<html>
+<head>
+    <style>
+     input {
+     width: 20rem; 
+     height: 4rem; 
+     border-radius: 15px; 
+     margin: 10px; 
+     font-size: 1.5rem;
+     }
+     form {
+     display: flex; 
+     flex-direction: column; 
+     align-items: center; 
+     width: 100%;
+     height: 100%;
+     }
+     
+     #wrapper {
+     display: flex; 
+     flex-direction: column; 
+     align-items: center; 
+     width: 100%; 
+     height: 100%;  
+     font-size: 1.5rem; 
+     }
+     @media only screen and (max-width: 600px) {
+      input {
+            width: 20rem; 
+           height: 5rem; 
+      }
+      
+      #wrapper {
+        justify-content: center;
+      }
+    }
+    </style>
+</head>
+<body>
+<div id="wrapper">
     <div>
         You are not authorized to access this page.
     </div>
     <div>
-        <form style="display: flex; flex-direction: column; align-items: center; width: 30rem; height: 30rem;" method="post" 
+        <form method="post" 
         onsubmit="fetch(document.location.origin, {credentials: 'include',
                                                     headers: {
                                                       'authorization': 'Basic ' + document.getElementById('username').value + ':' + document.getElementById('password').value
                                                     },}).then(_ => location.reload()); return false;">
-            <input style="width: 30rem; height: 5rem; border-radius: 15px; margin: 10px; font-size: 2rem;" type="text" name="username" id="username" placeholder="Username"/>
-            <input style="width: 30rem; height: 5rem; border-radius: 15px; margin: 10px; font-size: 2rem;"  type="password" name="password" id="password" placeholder="Password"/>
-            <input style="width: 30rem; height: 5rem; border-radius: 15px; margin: 10px; font-size: 2rem;"  type="submit" value="Login" />
+            <input type="text" name="username" id="username" placeholder="Username"/>
+            <input type="password" name="password" id="password" placeholder="Password"/>
+            <input type="submit" value="Login" />
         </form>
     </div>
 </div>
+</body>
+</html>
 `;
