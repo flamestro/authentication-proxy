1c975af6e8183e809673a24258b01c1bfc394e84
flamestro <noreplyflamestro@gmail.com>
2023-06-17 13:33:13 +0200

    Add viewport metatag

--- src/view/Login/loginView.ts
+++ src/view/Login/loginView.ts
@@ -6,6 +6,7 @@ export const loginView = `
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Authentication Proxy</title>
 
     <style>${loginViewStyle}</style>
